550052c21b51f2d7b09b7396385089a2014230f3
西田亮太 <24.ryota.sub@gmail.com>
2024-07-15 14:24:41 +0900

    formSectionの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import FormSection from "./components/FormSection";
+import AnswerSection from "./components/AnswerSection";
 
 function App() {
+  const generateResponse = (newQuestion, setNewQuestion) => {
+    
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>ChatGPT Clone</h1>
+        <div className='header-section'>
+
+        </div>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          I am an automated question and answer system, designed to assist you
+          in finding relevant information. You are welcome to ask me any queries
+          you may have, and I will do my utmost to offer you a reliable
+          response. Kindly keep in mind that I am a machine and operate solely
+          based on programmed algorithms.
         </p>
-        Learn React
+        <FormSection />
+        <AnswerSection />
       </header>
     </div>
   );
---
+++ src/components/FormSection.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+
+export default function FormSection({ generateResponse }) {
+  const [newQuestion, setNewQuestion] = useState<string>("");
+  console.log(newQuestion)
+  return (
+    <div>
+      <textarea
+        className="form-control"
+        placeholder="Ask me anyting"
+        cols={30}
+        rows={10}
+        value={newQuestion}
+        onChange={(e) => setNewQuestion(e.target.value)}
+      ></textarea>
+      <button
+        className='btn'
+        onClick={() => generateResponse(newQuestion, setNewQuestion)}
+      >Generate Response</button>
+    </div>
+
+  )
+}
