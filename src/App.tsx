394f38548c54e457c6fa7821a85c2ededcc1b71f
Ryota Nishida <24.ryota.sub@gmail.com>
2024-07-20 23:20:52 +0900

    更新

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,6 @@ function App() {
         <p>
           I am an automated question and answer system, designed to assist you
           in finding relevant information. You are welcome to ask me any queries
-          {/* you may have, and I will do my utmost to offer you a reliable
-          response. Kindly keep in mind that I am a machine and operate solely
-          based on programmed algorithms. */}
         </p>
         <FormSection />
         <AnswerSection />
