47b684053bab7a5a539764560235815e3201cea6
Ryota Nishida <24.ryota.sub@gmail.com>
2024-08-07 23:53:51 +0900

    更新

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>ChatGPT Clone</h1>
-        <div className='header-section'>
-        </div>
         <FormSection />
         <AnswerSection />
       </header>
